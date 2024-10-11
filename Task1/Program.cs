54617b8e7fab5bfb688e863cdf5e58b5dc9f2132
Mahir <mahir.cumaliyev12@gmail.com>
2024-10-11 23:55:26 +0400

    tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nums = "Ededler: ";
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    nums += i;
+                    nums += ", ";
+                }
+            }
+            Console.WriteLine(nums);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 85;
+            if (var < 65)
+            {
+                Console.WriteLine("Kəsildin");
+            }
+            else if (var < 85)
+            {
+                Console.WriteLine("Adi Diplom");
+            }
+            else if (var < 95)
+            {
+                Console.WriteLine("Şərəf Diplomu");
+            }
+            else
+            {
+                Console.WriteLine("Yüksək Şərəf Diplomu");
+            }
+        }
+    }
+}
